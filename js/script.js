75ccd2345fc8eb7ffc209ab62e65a5ee5d37d916
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-18 22:40:43 -0600

    started adding eventlistener

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
-
-
+var h2c = document.querySelectorAll('.center h2');
+var button = document.getElementById('entrybutton');
+button.addEventListener('click', function(){
+  alert('Gladys Toledo-Rodriguez: ');
+});
+button.addEventListener('click', function(){
+  h2c.innerHTML = '';
+});
 
 
 
