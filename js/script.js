701d522453cb040564942c13b0830efadae0c84a
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-18 23:28:41 -0600

    Hopefully getting text field input to output in alert

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
+var tf = document.getElementById('entryinput');
+
 var h2c = document.getElementsByTagName('h2')
 var button = document.getElementById('entrybutton');
+
 button.addEventListener('click', function(){
-  alert('Gladys Toledo-Rodriguez: ');
+  alert('Gladys Toledo-Rodriguez: ' + tf.value);
 });
+
 button.addEventListener('click', function(){
   h2c.innerHTML = 'Change text';
 });
